b4dbeed7344ed663d2a127fb495ddc0d6b891579
Akshay Sutar <58782848+akshaymsutar@users.noreply.github.com>
2021-06-21 16:34:44 +0530

    App name Change

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -5,7 +5,7 @@ import HeaderCartButton from './HeaderCartButton'
 const Header = props=>{
     return (<Fragment>
         <header className={classes.header}>
-            <h1>ReactMeals</h1>
+            <h1>Cafe Jupiter</h1>
             <HeaderCartButton onClick={props.onClick}/>
         </header>
         <div className={classes['main-image']}>
